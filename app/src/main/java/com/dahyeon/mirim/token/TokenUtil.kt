79999e759e96e59ac5c5e8b93580eb3b0c0666f5
JoMingyu <city7310@naver.com>
2018-11-18 18:43:23 +0900

    TokenUtil 추가

---
+++ app/src/main/java/com/dahyeon/mirim/token/TokenUtil.kt
@@ -0,0 +1,31 @@
+package com.dahyeon.mirim.token
+
+import android.content.*
+
+/**
+ * Created by root1 on 2017. 12. 15..
+ */
+object TokenUtil {
+    private fun getPref(context: Context): SharedPreferences{
+        val pref = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        return pref
+    }
+
+    fun saveToken(context: Context, token: String, isAccess: Boolean = true){
+        val editor = getPref(context).edit()
+        editor.putString(getKey(isAccess), token)
+        editor.apply()
+    }
+
+    fun removeToken(context: Context, isAccess: Boolean = true){
+        val editor = getPref(context).edit()
+        editor.remove(getKey(isAccess))
+        editor.apply()
+    }
+
+    fun getToken(context: Context, isAccess: Boolean = true): String{
+        return "JWT " + getPref(context).getString(getKey(isAccess), "")
+    }
+
+    private fun getKey(isAccess: Boolean): String = if (isAccess) "AccessToken" else "RefreshToken"
+}
