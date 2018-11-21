a8f1d70374b290fb5a8edba8d07dbabdb0115ef7
JoMingyu <city7310@naver.com>
2018-11-21 23:09:11 +0900

    MainActivity 리팩토링

--- app/src/main/java/com/mirimapp/mirim/activities/MainActivity.kt
+++ app/src/main/java/com/mirimapp/mirim/activities/MainActivity.kt
@@ -2,47 +2,32 @@ package com.mirimapp.mirim.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
+import android.view.View
 import com.dahyeon.mirim.R
 import com.mirimapp.mirim.util.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), View.OnClickListener {
+
+    val buttonIdActivityMapping = hashMapOf(
+        R.id.imagebutton_main_to_board_activity to Menu3BoardActivity::class.java,
+        R.id.imagebutton_main_to_dormitory_activity to Menu2DomitoryActivity::class.java,
+        R.id.imagebutton_main_to_market_activity to Menu4MarketActivity::class.java,
+        R.id.imagebutton_main_to_mypage_activity to Menu5MypageActivity::class.java,
+        R.id.imagebutton_main_to_school_activity to Menu1SchoolActivity::class.java
+    )
+
+    override fun onClick(v: View?) {
+        val viewID = v!!.id
+
+        if(buttonIdActivityMapping.containsKey(viewID)) {
+            startActivity(Intent(applicationContext, buttonIdActivityMapping[viewID]))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val menu1 = findViewById<ImageButton>(R.id.menu_btn1)
-        val menu2 = findViewById<ImageButton>(R.id.menu_btn2)
-        val menu3 = findViewById<ImageButton>(R.id.menu_btn3)
-        val menu4 = findViewById<ImageButton>(R.id.menu_btn4)
-        val menu5 = findViewById<ImageButton>(R.id.menu_btn5)
-
-        menu1.setOnClickListener {
-            val intent = Intent(applicationContext, Menu1SchoolActivity::class.java)
-            startActivity(intent)
-        }
-
-        menu2.setOnClickListener {
-            val intent = Intent(applicationContext, Menu2DomitoryActivity::class.java)
-            startActivity(intent)
-        }
-
-        menu3.setOnClickListener {
-            val intent = Intent(applicationContext, Menu3BoardActivity::class.java)
-            startActivity(intent)
-        }
-
-        menu4.setOnClickListener {
-            val intent = Intent(applicationContext, Menu4MarketActivity::class.java)
-            startActivity(intent)
-        }
-
-        menu5.setOnClickListener {
-            val intent = Intent(applicationContext, Menu5MypageActivity::class.java)
-            startActivity(intent)
-        }
 
         button_main_logout.setOnClickListener {
             removeToken(true)
