133ce3f14de31e93b35b0e288b72e9572fceb419
JoMingyu <city7310@naver.com>
2018-11-27 22:22:59 +0900

    onClickListener 제대로 안되는 현상 해결

--- app/src/main/java/com/mirimapp/mirim/activities/MainActivity.kt
+++ app/src/main/java/com/mirimapp/mirim/activities/MainActivity.kt
@@ -2,29 +2,11 @@ package com.mirimapp.mirim.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import com.dahyeon.mirim.R
 import com.mirimapp.mirim.util.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity(), View.OnClickListener {
-
-    val buttonIdActivityMapping = hashMapOf(
-        R.id.imagebutton_main_to_board_activity to Menu3BoardActivity::class.java,
-        R.id.imagebutton_main_to_dormitory_activity to Menu2DomitoryActivity::class.java,
-        R.id.imagebutton_main_to_market_activity to Menu4MarketActivity::class.java,
-        R.id.imagebutton_main_to_mypage_activity to Menu5MypageActivity::class.java,
-        R.id.imagebutton_main_to_school_activity to Menu1SchoolActivity::class.java
-    )
-
-    override fun onClick(v: View?) {
-        val viewID = v!!.id
-
-        if(buttonIdActivityMapping.containsKey(viewID)) {
-            startActivity(Intent(applicationContext, buttonIdActivityMapping[viewID]))
-        }
-    }
-
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,6 +16,22 @@ class MainActivity : BaseActivity(), View.OnClickListener {
             removeToken(false)
             startActivity(Intent(applicationContext, SigninActivity::class.java))
             finish()
+        }
+
+        imagebutton_main_to_board_activity.setOnClickListener {
+            startActivity(Intent(applicationContext, Menu3BoardActivity::class.java))
+        }
+        imagebutton_main_to_dormitory_activity.setOnClickListener {
+            startActivity(Intent(applicationContext, Menu2DomitoryActivity::class.java))
+        }
+        imagebutton_main_to_market_activity.setOnClickListener {
+            startActivity(Intent(applicationContext, Menu4MarketActivity::class.java))
+        }
+        imagebutton_main_to_mypage_activity.setOnClickListener {
+            startActivity(Intent(applicationContext, Menu5MypageActivity::class.java))
+        }
+        imagebutton_main_to_school_activity.setOnClickListener {
+            startActivity(Intent(applicationContext, Menu1SchoolActivity::class.java))
         }
     }
 }
